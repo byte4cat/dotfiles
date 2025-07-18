7c42e418d4af9b054bb43d50cbc84b65b4bbb416
yimincai <formosaneko.tw@gmail.com>
2025-07-18 23:34:52 +0800

    feat: add wallpapers

--- stow/common/scripts/.config/scripts/kitty_set_bg.sh
+++ stow/common/scripts/.config/scripts/kitty_set_bg.sh
@@ -2,10 +2,32 @@
 
 set -euo pipefail
 
-wallpaper_directories=(
-    "$HOME/.config/wallpapers/default"
-)
+base_wallpaper_directory="$HOME/.config/wallpapers/"
 
+# check if the base wallpaper directory exists
+if [ ! -d "$base_wallpaper_directory" ]; then
+    echo "Base wallpaper directory does not exist: $base_wallpaper_directory"
+    exit 1
+fi
+
+wallpaper_directories=()
+# Read the directories in the base wallpaper directory
+# This will allow us to use fzf to select a directory
+while IFS= read -r -d '' dir; do
+    wallpaper_directories+=("$dir")
+done < <(find "$base_wallpaper_directory" -mindepth 1 -maxdepth 1 -type d -print0)
+
+if [[ ${#wallpaper_directories[@]} -eq 0 ]]; then
+    echo "No wallpaper directories found in $base_wallpaper_directory"
+    exit 1
+fi
+
+for i in "${!wallpaper_directories[@]}"; do
+    # Remove the base wallpaper directory prefix for display
+    wallpaper_directories[$i]="${wallpaper_directories[$i]#$base_wallpaper_directory/}"
+done
+
+# Find all directories in the base wallpaper directory and use fzf to select one
 selected_dir=$(
     printf "%s\n" "${wallpaper_directories[@]}" | fzf --prompt="Select wallpaper directory: "
 )
@@ -24,7 +46,7 @@ else
 fi
 
 selected_image=$(
-    find "$wallpaper_directories" -mindepth 1 -maxdepth 1 -type f \
+    find "$base_wallpaper_directory$selected_dir" -mindepth 1 -maxdepth 1 -type f \
         \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.bmp" \) \
         -not -name '.DS_Store' |
         fzf --preview "$preview_cmd" \
