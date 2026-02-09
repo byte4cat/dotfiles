ca5104e5661cf07ee5923a34e4dbb0b17330b874
yimintsai <byte4cat@gmail.com>
2026-02-10 01:21:38 +0800

    feat: add snakes.nvim

--- config/nvim/.config/nvim/lua/neil/lsp-key-mapping.lua
+++ config/nvim/.config/nvim/lua/neil/lsp-key-mapping.lua
@@ -12,7 +12,7 @@ function M.setup(bufnr)
 	-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 	vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
+	-- vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
 
 	vim.keymap.set("n", "<C-d>", "<cmd>Lspsaga peek_definition<CR>", opts)
 	vim.keymap.set("n", "<leader>l", "<cmd>Lspsaga outline<CR>", opts)
---
+++ config/nvim/.config/nvim/lua/neil/plugins/snakes.lua
@@ -0,0 +1,139 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		-- replace telescope
+		picker = { enabled = true },
+
+		-- (replace nvim-notify)
+		notifier = { enabled = true, timeout = 2000 },
+
+		scroll = { enabled = true },
+
+		indent = {
+			enabled = true,
+			scope = { enabled = true }, -- 高亮當前作用域的線
+		},
+
+		-- focus
+		zen = { enabled = true },
+		dim = { enabled = true },
+
+		-- open git repo in browser
+		gitbrowse = { enabled = true },
+
+		-- cmd input
+		input = { enabled = true },
+
+		-- rename file
+		rename = { enabled = true },
+
+		statuscolumn = { enabled = true },
+
+		image = { enabled = false },
+	},
+	keys = {
+		-- Zen Mode & Dim
+		{
+			"<leader>zz",
+			function()
+				Snacks.zen()
+			end,
+			desc = "Toggle Zen Mode",
+		},
+		{
+			"<leader>zd",
+			function()
+				Snacks.dim()
+			end,
+			desc = "Toggle Dim Background",
+		},
+		-- Git Browse
+		{
+			"<leader>gB",
+			function()
+				Snacks.gitbrowse()
+			end,
+			desc = "Open in Browser",
+		},
+		-- Rename
+		{
+			"<leader>rn",
+			function()
+				Snacks.rename.rename_file()
+			end,
+			desc = "Rename File",
+		},
+		-- Notification History
+		{
+			"<leader>nh",
+			function()
+				Snacks.notifier.show_history()
+			end,
+			desc = "Notification History",
+		},
+		-- Picker
+		{
+			"<leader>pf",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find Files (Snacks)",
+		},
+		{
+			"<leader>ps",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep (Snacks)",
+		},
+		{
+			"<leader>pb",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Buffers (Snacks)",
+		},
+		{
+			"<leader>ph",
+			function()
+				Snacks.picker.help()
+			end,
+			desc = "Help Tags (Snacks)",
+		},
+		{
+			"<leader>pg",
+			function()
+				Snacks.picker.git_status()
+			end,
+			desc = "Git Status (Snacks)",
+		},
+		{
+			"<leader>pc",
+			function()
+				Snacks.picker.git_log_file()
+			end,
+			desc = "Git Buffer Commits (Snacks)",
+		},
+		{
+			"<leader>pl",
+			function()
+				Snacks.picker.git_log()
+			end,
+			desc = "Git Log",
+		},
+	},
+	config = function(_, opts)
+		-- only enabled image in markdown file
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "markdown",
+			callback = function()
+				opts.image.enabled = true
+				require("snacks.image").setup(opts.image)
+			end,
+		})
+
+		require("snacks").setup(opts)
+	end,
+}
