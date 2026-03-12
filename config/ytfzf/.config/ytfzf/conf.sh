e0d2abecb0136b1b8af2b0b582c2f2cdff82388f
yimintsai <byte4cat@gmail.com>
2026-03-12 09:44:12 +0800

    feat: update settings

---
+++ config/ytfzf/.config/ytfzf/conf.sh
@@ -0,0 +1,16 @@
+is_loop=1
+thumbnail_viewer=kitty
+show_thumbnails=1
+async_thumbnails=1
+scrape="yt"
+pages_to_scrape=3
+search_result_type="all"
+search_region="TW"
+show_formats=0
+invidious_instance="https://yewtu.be"
+shortcut_binds="Enter,double-click,$download_shortcut,$video_shortcut,$audio_shortcut,$detach_shortcut,$print_link_shortcut,$show_formats_shortcut,$info_shortcut,$search_again_shortcut,$custom_shortcut_binds"
+nsfw=true
+# 讓 ytfzf 啟動 mpv 時帶入全螢幕參數
+video_pref="--fs"
+video_format_menu_opts="--tac"
+ytdl_opts="--write-auto-subs --sub-langs 'zh.*,en.*' --config-location ~/.config/yt-dlp/config"
