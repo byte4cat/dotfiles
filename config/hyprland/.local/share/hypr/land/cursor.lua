5a6557071a129dfdfd02d1956fb68dfa939901e0
yimintsai <byte4cat@gmail.com>
2026-05-19 16:16:26 +0800

    fix: hyprland lua config errors

--- config/hyprland/.local/share/hypr/land/cursor.lua
+++ config/hyprland/.local/share/hypr/land/cursor.lua
@@ -9,32 +9,32 @@ hl.config({
 	},
 })
 
-hl.config({
-	plugin = {
-		["dynamic-cursors"] = {
-			enabled = true,
-			mode = "tilt",
-			threshold = 2,
-
-			tilt = {
-				limit = 5000,
-				["function"] = "negative_quadratic",
-				full_tilt = 60,
-			},
-
-			shake = {
-				enabled = true,
-				nearest = true,
-				threshold = 5.0,
-				base = 4.0,
-				speed = 4.0,
-				timeout = 500,
-			},
-
-			hyprcursor = {
-				enabled = true,
-				nearest = true,
-			},
-		},
-	},
-})
+-- hl.config({
+-- 	plugin = {
+-- 		["dynamic-cursors"] = {
+-- 			enabled = true,
+-- 			mode = "tilt",
+-- 			threshold = 2,
+--
+-- 			tilt = {
+-- 				limit = 5000,
+-- 				["function"] = "negative_quadratic",
+-- 				full_tilt = 60,
+-- 			},
+--
+-- 			shake = {
+-- 				enabled = true,
+-- 				nearest = true,
+-- 				threshold = 5.0,
+-- 				base = 4.0,
+-- 				speed = 4.0,
+-- 				timeout = 500,
+-- 			},
+--
+-- 			hyprcursor = {
+-- 				enabled = true,
+-- 				nearest = true,
+-- 			},
+-- 		},
+-- 	},
+-- })
